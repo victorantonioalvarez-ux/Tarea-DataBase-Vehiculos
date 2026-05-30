0af7e53a7eed180ff29ec8653eab85c433ee9699
Victor <victorantonioalvarez@paucasesnovescifp.cat>
2026-05-30 20:05:44 +0200

    feat: Creado sistema para cargar los datos de la Database

--- scripts.js
+++ scripts.js
@@ -1,5 +1,25 @@
 import { DataBase } from './db.js';
-import {Coche} from './coche.js';
-import {Moto} from './moto.js';
-import {Camion} from './camion.js';
+import { Coche } from './coche.js';
+import { Moto } from './moto.js';
+import { Camion } from './camion.js';
 
+const lista = document.getElementById('llistaVehicles');
+const db = new DataBase();
+
+
+async function cargarDatos(){
+    lista.innerHTML = `<div>Cargando datos...</div>`; 
+    try{
+        const respuesta = await new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(db.datosIniciales()); }, 2000);
+        });
+        mostrarVehiculos(respuesta)
+    } catch (error) {
+       lista.innerHTML = `<div>Error: ${error}</div>`;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('carregarVehicles').addEventListener("click", cargarDatos);
+});

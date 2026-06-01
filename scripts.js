e064efbe4c96581ad713b6a68166f21dd28efefe
Victor <victorantonioalvarez@paucasesnovescifp.cat>
2026-06-01 10:05:14 +0200

    feat: creado metodo para añadir vvehiculo <descripcion> He modificado tambien mostrar vehiculo porque no se mostraban todos los campos que introduciamos del formulario, salia undefiend

--- scripts.js
+++ scripts.js
@@ -23,15 +23,32 @@ async function cargarDatos(){
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('carregarVehicles').addEventListener("click", cargarDatos);
+    document.getElementById('carregarVehicles').addEventListener('click', cargarDatos);
     document.getElementById('tipusVehicle').addEventListener('change', cambiarTipo);
+    document.getElementById('afegirVehicle').addEventListener('click', addVehiculo);
 });
 
 function mostrarVehiculos(vehiculos){
     lista.innerHTML = "";
     vehiculos.forEach(vehiculo => {
         const div = document.createElement("div");
-        div.innerHTML = `<div>${vehiculo.tipo} | ${vehiculo.marca} | ${vehiculo.modelo} | ${vehiculo.año} | ${vehiculo.extra}</div>`;
+        let type;
+        let extra;
+        if(vehiculo instanceof Coche){
+            extra = vehiculo.puertas;
+            type = "Coche";
+        } else if(vehiculo instanceof Moto){
+            extra = vehiculo.tipo;
+            type = "Moto";
+        } else if(vehiculo instanceof Camion){
+            extra = vehiculo.peso;
+            type = "Camion";
+        } else {
+            extra = vehiculo.extra;
+            type = vehiculo.type;
+        }
+
+        div.innerHTML = `<div>${type} | ${vehiculo.marca} | ${vehiculo.modelo} | ${vehiculo.año} | ${extra}</div>`;
         lista.appendChild(div)
     });
 }
@@ -48,4 +65,38 @@ function cambiarTipo(){
     } 
     textoExtra.innerHTML = extra;
     inputExtra.placeholder = extra;
+}
+
+async function addVehiculo(){
+    const tipo = document.getElementById('tipusVehicle').value;
+    const marca = document.getElementById('marca').value;
+    const modelo = document.getElementById('model').value;
+    const año = document.getElementById('any').value;
+    const extra = document.getElementById('extra').value;
+    let nuevoVehiculo;
+
+    if(!marca || !modelo || !año || !extra) {
+        alert("Todos los campos son obligatorios!!!");
+        return;
+    }
+
+    if(tipo === "Cotxe"){
+        nuevoVehiculo = new Coche(marca ,modelo, año, extra);
+    }else if(tipo === "Moto"){
+        nuevoVehiculo = new Moto(marca, modelo, año, extra);
+    }else {
+        nuevoVehiculo = new Camion(marca, modelo, año, extra);
+    }
+
+    try{
+        const promesa = await new Promise ((resolve, reject) => {
+            setTimeout(() => {
+                db.agregar(nuevoVehiculo);
+                resolve(nuevoVehiculo);
+            }, 1000);
+        });
+        mostrarVehiculos(db.listaVehiculos);
+    }catch(error) {
+        alert(error);
+    }
 }
